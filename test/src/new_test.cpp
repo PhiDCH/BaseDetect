5b846ad3698da07a7cbfa152403ad5739fe28ec7
phidch <phi9b2@gmail.com>
2022-07-12 19:44:03 +0700

    modify yolox

--- test/src/new_test.cpp
+++ test/src/new_test.cpp
@@ -11,14 +11,21 @@ class A {
 
 class B : public A {
     public:
-    B(int a) : A(a) {};
+    int y;
+    B(int a, int b=1) : A(a) {
+        y = b;
+    };
     // ~B() {cout << "~B" << endl;};
 };
+
+void tmp(int a, int b = 1) {
+    cout << a  << b << endl;
+}
 
 int main() {
     if (1) {
         B b(1);
     }
-
+    tmp(1);
     return 0;
 }
